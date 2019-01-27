c117bb531c7605a611fb39b522257632b8c106cd
AlexandrS17 <43346969+AlexandrS17@users.noreply.github.com>
2019-01-27 21:30:20 +0300

    Update learning.py

--- learning.py
+++ learning.py
@@ -16,12 +16,7 @@ class WordsFill:
         self.morph = pymorphy2.MorphAnalyzer()
 
         self.text0 = text[0]
-        # self.text = list(map(lambda x: x.split(), text))
-        # print(self.text)
         self.knowledge = {}
-        '''for pr in self.text:
-            for w in pr:
-                print(self.morph.parse(w))'''
         self.fit()
 
     def fit(self):
@@ -33,30 +28,26 @@ class WordsFill:
                 self.knowledge[symbol] = self.knowledge.get(symbol, '') + (sentence[i+1] + '|')\
                     if sentence[i+1] not in self.knowledge.get(symbol, '') else self.knowledge.get(symbol)
 
-        # print(self.knowledge)
         KNOWLEDGE = self.knowledge
         return self.knowledge
 
 
 class WordsGenerate:
     '''подключаем известную из ЯндексЛицея (: библиотеку Pymorphy2 для работы со словами;
+       pymorphy пока бесполезна, но с её помощью можно будет(потооом) сделать 
     '''
     def __init__(self, l):
         self.len_sen = l
         self.knowledge = KNOWLEDGE
         super().__init__()
 
-        # print(self.knowledge)
         self.generate()
 
     def generate(self):
-        # Важно посмотреть print(self.knowledge.keys())
         word = random.choice(list(self.knowledge.keys()))
-        # print(word)
-        # r = range(self.len_sen)
         line = word[0].upper() + word[1:] + ' '
+        
         for _ in range(self.len_sen-1):
-            # print(self.knowledge[word][:-1].split('|'))
             try:
                 word = random.choice(self.knowledge[word][:-1].split('|'))
             except KeyError:
@@ -84,7 +75,6 @@ def produce_txt(*f):
 
 
 all_files = WordsFill(produce_txt(file1, file2, file3))
-# print('!!!', all_files.fit())
 
 for _ in range(5):
     print("После ввода длины строки Вы увидите гениальное предложение!!")
